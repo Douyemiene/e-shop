5b71b136802782c340d0da3c6c7721581d1b4e2f
INSPIRON <viktormiene.vm@gmail.com>
2019-08-23 06:17:19 -0700

    completed styles for homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Directory from '../../components/directory/directory.compenent'
+import './homepage.styles.scss'
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+export default Homepage
