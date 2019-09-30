732670fd876056dfb8a9bdbb14a2055797af8dfa
INSPIRON <viktormiene.vm@gmail.com>
2019-09-30 09:18:59 -0700

    moved our directory and shop data into its own reducer, created respective selector and updated corresponding components with new redux flow for directory/shop

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import "./collection.styles.scss";
+
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title"> {title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+//1st prop is the overall reudux state,
+//the second is called ownProp (the prop of the component we wrap in connect)
+//mapstateToProps 2nd parameter (optional)
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 //a function from redux that combines individual reducersto form the root reducer
-export default combineReducers({
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+export default persistReducer(persistConfig, rootReducer);
